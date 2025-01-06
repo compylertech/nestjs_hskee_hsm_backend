0b3de45a8c924327bae952eba2c9cb7f82009a2e
Daniel Quaidoo <daniel.quaidoo@gmail.com>
2025-01-06 23:21:38 +0000

    add users

--- apps/aleva-api-gateway/src/forms/modules/questionnaire/questionnaire.service.ts
+++ apps/aleva-api-gateway/src/forms/modules/questionnaire/questionnaire.service.ts
@@ -45,7 +45,7 @@ export class QuestionnaireService {
   async fetchGroupedQuestionnaireData(pageOptionsDto: PageOptionsDto, entityId?: string[]): Promise<any[]> {
     // fetch questionnaire responses
     const responses = await this.questionnaireClient
-      .send<any[] | PageDto<any>>(QUESTIONNAIRE_PATTERN.GET_ENTITY_RESPONSES, { pageOptionsDto, entityId })
+      .send<any[] | PageDto<any[]>>(QUESTIONNAIRE_PATTERN.GET_ENTITY_RESPONSES, { pageOptionsDto, entityId })
       .toPromise();
 
     if (!responses?.data?.length) {
